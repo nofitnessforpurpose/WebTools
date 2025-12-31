d8d352c088f5db1e74a19d183bc5199a851d5764
nofitnessforpurpose <nofitnessforpurpose@gmail.com>
2025-12-31 15:29:31 +0000

    Delete OPKEditor3/src directory

--- OPKEditor3/src/ui/editors/HeaderlessFileEditor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//------------- Headerless File editor --------------------
-
-function HeaderlessFileEditor(editorelement, callback) {
-    HexEditor.call(this, editorelement, callback, [0]);
-}
-HeaderlessFileEditor.prototype = Object.create(HexEditor.prototype);
-
