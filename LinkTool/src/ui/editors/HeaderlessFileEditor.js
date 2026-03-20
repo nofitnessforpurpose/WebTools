4b38b69b982ad6649f9baae96b2ba4b939251da9
nofitnessforpurpose <nofitnessforpurpose@gmail.com>
2026-03-20 14:56:13 +0000

    Delete LinkTool/src directory

--- LinkTool/src/ui/editors/HeaderlessFileEditor.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-
-function HeaderlessFileEditor(editorelement,callback){
-HexEditor.call(this,editorelement,callback,[0]);
-}
-HeaderlessFileEditor.prototype=Object.create(HexEditor.prototype);
